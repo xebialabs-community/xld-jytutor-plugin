80ca11ca802083bd6802d347180a9968d012de81
amitmohleji <amit.mohleji@gmail.com>
2015-01-29 18:59:25 -0500

    minor update for loop based code snippets

--- src/main/python/restapi/jytutor.py
+++ src/main/python/restapi/jytutor.py
@@ -1,7 +1,6 @@
 import org.codehaus.jettison.json.JSONObject
 import os
 
-commands = request.entity['commands'].split("\n")
-for item in commands:
-	exec item
 response.entity = request.entity
+exec request.entity['commands']
+
